83f70b145355aa9944fb2c68e436870f43ce6eca
Peter Tumulty <ptumulty923@gmail.com>
2022-06-06 21:45:47 -0400

    fixed label switch function

--- src/utils/helpters.ts
+++ src/utils/helpters.ts
@@ -1,9 +1,13 @@
 export const generateLabelColors = (type: string) => {
   switch(type){
-    case 'CSS' || 'Intro to Web Development':
+    case 'Intro to Web Development':
      return 'bg-green-100 text-green-800';
-    case 'JavaScript' || 'Intermediate CSS: Flexbox and Grid':
+    case 'CSS':
+        return 'bg-green-100 text-green-800';
+    case 'Intermediate CSS: Flexbox and Grid':
      return 'bg-blue-100 text-blue-800';
+    case 'JavaScript':
+      return 'bg-blue-100 text-blue-800';
     case 'JavaScript & The DOM':
      return 'bg-yellow-100 text-yellow-800';
    default:
